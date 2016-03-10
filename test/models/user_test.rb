7c98def4593f74970a5117ca7ac67566e60c79d7
macsteps <macsteps@gmail.com>
2016-03-10 11:17:15 -0800

    add users controller and user model.

---
+++ test/models/user_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(name: "Test User", email: "testuser@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+end

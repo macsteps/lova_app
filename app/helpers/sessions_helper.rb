d7d4dfda3ce8b4c93652f64097e66c8a621c91f6
macsteps <macsteps@gmail.com>
2016-03-11 09:48:58 -0800

    add logged_in? method, modify header to differ logged in vs not logged in, add bootstrap javascript for dropdown menu behavior.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,18 @@
 module SessionsHelper
 
+  # To log in, store user_id in session hash
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
+  # Returns the current logged in user, if it exists.
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Returns true if user logged in, or false
+  def logged_in?
+    !current_user.nil?
+  end
+
 end

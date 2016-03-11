cc8df534c59a59c0e6943b91c1b187a60c333e07
macsteps <macsteps@gmail.com>
2016-03-11 11:13:54 -0800

    add log out method to destroy session, and test.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,7 +15,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "valid log in attempt" do
+  test "valid log in attempt followed by log out" do
     get login_path
     assert_template 'sessions/new'
     post login_path, session: { email: @user.email, password: "password" }
@@ -25,6 +25,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
 end
